1238f74410cadde6be8102dbcb7c2ee94cc1ab24
Aya Zaber <133893940+Ayaazr@users.noreply.github.com>
2024-12-15 23:57:03 +0100

    Add files via upload

---
+++ LangChain4j-mainTP2/src/main/java/com/example/langchain4jmaintp2/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.langchain4jmaintp2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
